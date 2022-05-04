6802513daa05c7e39326daa52bc5b0a56f45c4d8
badri0909 <singuveerabhadrarao@gmail.com>
2022-05-04 09:42:34 +0530

    add .

---
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Program");
+        }
+    }
+}
